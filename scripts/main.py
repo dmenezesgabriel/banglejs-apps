dfea425173561e55a5a726d18e937fd1bef323d9
Gabriel <dmenezes.gabriel@gmail.com>
2021-09-10 23:21:54 -0300

    update gh-pages deploy test 6

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,14 @@
 import os
 
 print(os.listdir(os.getcwd()))
+
+REMOVE_LIST = ["apps", "README.md"]
+
+for item in REMOVE_LIST:
+    if os.path.exists(item):
+        os.remove(item)
+    else:
+        print("The folder does not exist")
+
+print(os.listdir(os.getcwd()))
+
